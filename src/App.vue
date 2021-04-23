d8c7c4d2657f3dedb8d828b3f1c9e61909a44883
byun618 <byun618@gmail.com>
2021-04-23 19:06:28 +0900

    build vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <div>
+      <router-view class="container main"></router-view>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+import router from "./router";
+import "regenerator-runtime/runtime";
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$eventBus = new Vue();
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
